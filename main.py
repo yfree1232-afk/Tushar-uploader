92901c10369347e38922054e9e9efa08a042848f
yfree1232-afk <yfree1232@gmail.com>
2026-03-01 09:40:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -562,12 +562,14 @@ async def upload(bot: Client, m: Message):
     await input6.delete(True)
     await editable.delete()
 
-    #thumb = input6.text
-    #if thumb.startswith("http://") or thumb.startswith("https://"):
-        #getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
-        #thumb = "thumb.jpg"
-    #else 
-    #thumb == "no"
+thumb = "no"
+failed_count = 0
+
+if len(links) == 1:
+    count = 1
+else:
+    count = int(raw_text)
+    
     failed_count =0
     if len(links) == 1:
         count = 1
