aae3254baefbed385f436b58d7957251f1a9d164
yfree1232-afk <yfree1232@gmail.com>
2026-03-01 09:37:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -566,8 +566,8 @@ async def upload(bot: Client, m: Message):
     #if thumb.startswith("http://") or thumb.startswith("https://"):
         #getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
         #thumb = "thumb.jpg"
-    else:
-        thumb == "no"
+    #else 
+    #thumb == "no"
     failed_count =0
     if len(links) == 1:
         count = 1
